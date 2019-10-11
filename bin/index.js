9c17a19492a3063eee2d5ca5b29bf6ce25efc18b
Giancarlo_Maffezzolli <39268451+GiancarloM1@users.noreply.github.com>
2019-10-10 22:30:45 -0300

    att

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Cidade_1 = require("./Cidade");
 var Area_1 = require("./Area");
 var Casas_1 = require("./Casas/Casas");
+var Edificio_1 = require("./Edificios/Edificio");
 var casa1_1 = require("./Casas/casa1");
+var Edificio1_1 = require("./Edificios/Edificio1");
 var cidade = new Cidade_1.Cidade();
 /*------------------
 --------------------*/
 var casas = new Area_1.Area(Casas_1.Casas, 1000);
-//let edificio = new Area(Edificio,5000);
+var edificio = new Area_1.Area(Edificio_1.Edificio, 5000);
 //let industria = new Area(Industria,10000);
 /*------------------
 --------------------*/
@@ -18,7 +20,21 @@ casa1.setTamanho(500);
 var casa2 = new casa1_1.Casa1();
 casa2.setNome("casa2");
 casa2.setTamanho(600);
+var casa3 = new casa1_1.Casa1();
+casa2.setNome("casa3");
+casa2.setTamanho(200);
+var edificio1 = new Edificio1_1.Edificio1();
+edificio1.setNome("ed1");
+edificio1.setTamanho(4000);
+var edificio2 = new Edificio1_1.Edificio1();
+edificio1.setNome("ed2");
+edificio1.setTamanho(3000);
 cidade.addArea(casas);
+cidade.addArea(edificio);
 cidade.addImovel(casa1);
 cidade.addImovel(casa2);
+cidade.addImovel(casa3);
+cidade.addImovel(edificio1);
+cidade.addImovel(edificio2);
 cidade.listaImoveis(casas);
+cidade.listaImoveis(edificio);
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Casas } from "./Casas/Casas";
 import { Edificio} from "./Edificios/Edificio";
 import { Industria} from "./Industria/Industria";
 import { Casa1} from "./Casas/casa1"; 
+import { Edificio1 } from "./Edificios/Edificio1";
 
 let cidade = new Cidade();
 
@@ -11,7 +12,7 @@ let cidade = new Cidade();
 --------------------*/
 
 let casas = new Area(Casas,1000);
-//let edificio = new Area(Edificio,5000);
+let edificio = new Area(Edificio,5000);
 //let industria = new Area(Industria,10000);
 
 /*------------------
@@ -25,12 +26,29 @@ let casa2 = new Casa1();
 casa2.setNome("casa2");
 casa2.setTamanho(600);
 
+let casa3 = new Casa1();
+casa2.setNome("casa3");
+casa2.setTamanho(200);
+
+let edificio1 = new Edificio1();
+edificio1.setNome("ed1");
+edificio1.setTamanho(4000);
+
+
+let edificio2 = new Edificio1();
+edificio1.setNome("ed2");
+edificio1.setTamanho(3000);
 
 cidade.addArea(casas);
+cidade.addArea(edificio);
 
 cidade.addImovel(casa1);
 cidade.addImovel(casa2);
+cidade.addImovel(casa3);
+cidade.addImovel(edificio1);
+cidade.addImovel(edificio2);
 
 cidade.listaImoveis(casas);
+cidade.listaImoveis(edificio);
 
 
